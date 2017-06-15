01c3337a4147d325cb6e7de7f5d2342090fc1808
Hyeon Kim <simnalamburt@gmail.com>
2017-06-15 22:14:39 +0900

    Use the mustache template engine

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -1,8 +1,21 @@
+import com.google.common.collect.ImmutableMap;
+
+import spark.ModelAndView;
+import spark.template.mustache.MustacheTemplateEngine;
+
 import static spark.Spark.*;
 
 public class App {
+    private static long counter = 0;
+
     public static void main(String[] args) {
-        get("/", (req, res) -> "Hello World");
+        get("/", (req, res) -> {
+            counter += 1;
+
+            return new ModelAndView(ImmutableMap.of(
+                        "counter", counter),
+                    "index.mustache");
+        }, new MustacheTemplateEngine());
 
         System.out.println("\n\nSee \u001B[33mhttp://localhost:4567\u001B[0m\n\n");
     }
