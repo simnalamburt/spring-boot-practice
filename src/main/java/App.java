f814070e252a50ef0c64f53adff92ba490016021
Hyeon Kim <simnalamburt@gmail.com>
2017-06-15 21:07:18 +0900

    Hello, Spark!

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -1,9 +1,9 @@
-public class App {
-    public String getGreeting() {
-        return "Hello world.";
-    }
+import static spark.Spark.*;
 
+public class App {
     public static void main(String[] args) {
-        System.out.println(new App().getGreeting());
+        get("/", (req, res) -> "Hello World");
+
+        System.out.println("\n\nSee \u001B[33mhttp://localhost:4567\u001B[0m\n\n");
     }
 }
