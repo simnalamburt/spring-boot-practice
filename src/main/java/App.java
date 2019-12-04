c969533bc85be1893225e66d631afbc37ce076c4
Hyeon Kim <simnalamburt@gmail.com>
2019-12-04 23:53:30 +0900

    Intentionally used containesKey+put instead of putIfAbsent
    Intentionally used containesKey+put instead of putIfAbsent to test
    Amazon CodeGuru

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -1,3 +1,4 @@
+import java.util.concurrent.ConcurrentHashMap;
 import com.google.common.collect.ImmutableMap;
 
 import spark.ModelAndView;
@@ -6,16 +7,38 @@ import spark.template.mustache.MustacheTemplateEngine;
 import static spark.Spark.*;
 
 public class App {
-    private static long counter = 0;
+    private static ConcurrentHashMap<String, Integer> map = new ConcurrentHashMap<String, Integer>(1);
 
     public static void main(String[] args) {
         staticFileLocation("public");
 
         get("/", (req, res) -> {
+            // Intentionally wrote a poor code to test Amazon CodeGuru
+            int counter;
+            if (map.containsKey("counter")) {
+                counter = map.get("counter");
+            } else {
+                map.put("counter", 0);
+                counter = 0;
+            }
+
             return new ModelAndView(ImmutableMap.of("counter", counter), "index.mustache");
         }, new MustacheTemplateEngine());
 
-        post("/roar", (req, res) -> String.format("%d", ++counter));
+        post("/roar", (req, res) -> {
+            // Intentionally wrote a poor code to test Amazon CodeGuru
+            int counter;
+            if (map.containsKey("counter")) {
+                counter = map.get("counter");
+                ++counter;
+                map.put("counter", counter);
+            } else {
+                map.put("counter", 1);
+                counter = 1;
+            }
+
+            return String.format("%d", counter);
+        });
 
         System.out.println("\n\nSee \u001B[33mhttp://localhost:4567\u001B[0m\n\n");
     }
