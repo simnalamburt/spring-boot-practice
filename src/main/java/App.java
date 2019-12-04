7b6b3e8bd315fe3fe318f000a29fe2d91e9312b6
Hyeon Kim <simnalamburt@gmail.com>
2019-12-04 23:44:58 +0900

    Revert "Intentionally do bubble sort"
    This reverts commit c7d9d2536417df5fdcc5f6903a66067610ecdae4.

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -1,4 +1,3 @@
-import java.util.Arrays;
 import com.google.common.collect.ImmutableMap;
 
 import spark.ModelAndView;
@@ -17,34 +16,6 @@ public class App {
         }, new MustacheTemplateEngine());
 
         post("/roar", (req, res) -> String.format("%d", ++counter));
-
-        // "input" is list of string deliminated with comma (,)
-        // Example:
-        //   a,d,b,e,c
-        //   dog,cat,bear
-        //
-        // It returns sorted list of "input"
-        // Example:
-        //   a,b,c,d,e
-        //   bear,cat,dog
-        get("/sort/:input", (req, res) -> {
-            var input = req.params(":input");
-            var words = input.split(",");
-
-            // NOTE: Intentionally written in poor bubble sort to test Amazon CodeGuru
-            var len = words.length;
-            for (var i = 0; i < len-1; i++) {
-                for (var j = 0; j < len-i-1; j++) {
-                    if (words[j].compareTo(words[j+1]) > 0) {
-                        var tmp = words[j];
-                        words[j] = words[j+1];
-                        words[j+1] = tmp;
-                    }
-                }
-            }
-
-            return String.join(",", words);
-        });
 
         System.out.println("\n\nSee \u001B[33mhttp://localhost:4567\u001B[0m\n\n");
     }
