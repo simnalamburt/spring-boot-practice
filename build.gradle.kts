7d06ec33e0127e01272016d5b97105f63a0dedc4
Hyeon Kim <simnalamburt@gmail.com>
2020-12-21 20:49:36 +0900

    build.gradle.kts: Add some test dependencies

--- build.gradle.kts
+++ build.gradle.kts
@@ -10,4 +10,13 @@ repositories {
 dependencies {
     implementation("org.springframework.boot:spring-boot-starter-web:2.4.1")
     implementation("org.springframework.boot:spring-boot-starter-mustache:2.4.1")
+    testImplementation("org.springframework.boot:spring-boot-starter-test") {
+        exclude(group="org.junit.vintage", module="junit-vintage-engine")
+    }
+}
+
+tasks {
+    test {
+        useJUnitPlatform()
+    }
 }
